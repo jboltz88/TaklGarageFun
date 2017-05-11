9cad75e4a4083230262dd90a02fcb40a3421cb9c
Charles Smith <charles.smith@takl.com>
2017-05-11 08:32:01 -0500

    Removed main front page stuff

--- spec/features/visitors/about_page_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'About page' do
-  scenario 'Visit the about page' do
-    visit 'pages/about'
-    expect(page).to have_content 'About the Website'
-  end
-end
--- spec/features/visitors/home_page_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'Home page' do
-  scenario 'visit the home page' do
-    visit root_path
-    expect(page).to have_content 'Welcome'
-  end
-end
